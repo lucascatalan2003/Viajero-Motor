05ae59f7e34abf6b37bdb4fb9579d075b40b9e6e
Lucas Catalan <ramirocatalan03@gmail.com>
2024-02-12 20:48:30 -0300

    Primer proyecto

---
+++ js/main.js
@@ -0,0 +1,169 @@
+
+let vehiculo1 = 20000
+let vehiculo2 = 27000
+let vehiculo3 = 27000
+let vehiculo4 = 24000
+
+
+function saludar(saludo) {
+
+    console.log("Bienvenido/a" + " " + nombre);
+}
+let nombre = prompt("Diguite su nombre:")
+saludar(nombre)
+let eleccion = prompt("Por favor" + " " + nombre + "  " + "seleccione su modelo de auto: \n 1: Corsa Classic \n 2: Fiat Cronos \n 3: Fiat Siena \n 4: Gol Power \n x:Salir")
+let dia = prompt("Seleccione un dia de la semana \n 1:Lunes\n 2:Martes\n 3:Miercoles\n 4:Jueves\n 5:Viernes\n 6:Sabado \n 7:Domingo \n 8:salir")
+
+while (eleccion != "x") {
+    if (eleccion == "1") {
+        alert("Selecciono el modelo Corsa Classic, su monto a pagar es $" + vehiculo1 + ". Lo podra pasar a buscar el dia" + dia + ".")
+        while (dia != 8) {
+            if (dia == "1") {
+                alert("Y lo tendra por el dia lunes.")
+                break
+            } else if (dia == "2") {
+                alert("Y lo tendra por el dia Martes.")
+                break
+            } else if (dia == "3") {
+                alert("Y lo tendra por el dia Miercoles.")
+                break
+            } else if (dia == "4") {
+                alert("Y lo tendra por el dia Jueves.")
+                break
+            } else if (dia == "5") {
+                alert("Y lo tendra por el dia Viernes.")
+                break
+            } else if (dia == "6") {
+                alert("Y lo tendra por el dia Sabado.")
+                break
+            } else if (dia == "7") {
+                alert("Y lo tendra por el dia Domingo.")
+                break
+            } else  {
+                alert("Y lo tendra por el dia Martes.")
+                break}
+
+
+
+
+            }
+            break
+            } else if (eleccion == "2") {
+                alert("Selecciono el modelo Fiat Cronos,su monto a pagar es $" + vehiculo2 + ".")
+                while (dia != 8) {
+                    if (dia == "1") {
+                        alert("Y lo tendra por el dia lunes.")
+                        break
+                    } else if (dia == "2") {
+                        alert("Y lo tendra por el dia Martes.")
+                        break
+                    } else if (dia == "3") {
+                        alert("Y lo tendra por el dia Miercoles.")
+                        break
+                    } else if (dia == "4") {
+                        alert("Y lo tendra por el dia Jueves.")
+                        break
+                    } else if (dia == "5") {
+                        alert("Y lo tendra por el dia Viernes.")
+                        break
+                    } else if (dia == "6") {
+                        alert("Y lo tendra por el dia Sabado.")
+                        break
+                    } else if (dia == "7") {
+                        alert("Y lo tendra por el dia Domingo.")
+                        break
+                    } else  {
+                        alert("Y lo tendra por el dia Martes.")
+                        break}
+                    
+                    }
+                    break
+            } else if (eleccion == "3") {
+                alert("Selecciono el modelo Fiat Siena,su monto a pagar es $" + vehiculo3 + ".")
+                while (dia != 8) {
+                    if (dia == "1") {
+                        alert("Y lo tendra por el dia lunes.")
+                        break
+                    } else if (dia == "2") {
+                        alert("Y lo tendra por el dia Martes.")
+                        break
+                    } else if (dia == "3") {
+                        alert("Y lo tendra por el dia Miercoles.")
+                        break
+                    } else if (dia == "4") {
+                        alert("Y lo tendra por el dia Jueves.")
+                        break
+                    } else if (dia == "5") {
+                        alert("Y lo tendra por el dia Viernes.")
+                        break
+                    } else if (dia == "6") {
+                        alert("Y lo tendra por el dia Sabado.")
+                        break
+                    } else if (dia == "7") {
+                        alert("Y lo tendra por el dia Domingo.")
+                        break
+                    } else  {
+                        alert("Y lo tendra por el dia Martes.")
+                        break}
+                        
+                    }
+                    break
+            } else if (eleccion == "4") {
+                alert("Selecciono el modelo Gol Power,su monto a pagar es $" + vehiculo4 + ".")
+                while (dia != 8) {
+                    if (dia == "1") {
+                        alert("Y lo tendra por el dia lunes.")
+                        break
+                    } else if (dia == "2") {
+                        alert("Y lo tendra por el dia Martes.")
+                        break
+                    } else if (dia == "3") {
+                        alert("Y lo tendra por el dia Miercoles.")
+                        break
+                    } else if (dia == "4") {
+                        alert("Y lo tendra por el dia Jueves.")
+                        break
+                    } else if (dia == "5") {
+                        alert("Y lo tendra por el dia Viernes.")
+                        break
+                    } else if (dia == "6") {
+                        alert("Y lo tendra por el dia Sabado.")
+                        break
+                    } else if (dia == "7") {
+                        alert("Y lo tendra por el dia Domingo.")
+                        break
+                    } else  {
+                        alert("Y lo tendra por el dia Martes.")
+                        break}
+                        
+                    }
+                    break
+            } else {
+                alert("Lo esperamos pronto.")
+            }
+        }
+//let dia = prompt("Seleccione un dia de la semana \n 1:Lunes\n 2:Martes\n 3:Miercoles\n 4:Jueves\n 5:Viernes\n 6:Sabado \n 7:Domingo \n 8:salir")
+
+
+
+
+//case " 1:Corsa Classic":
+//prompt("Selecciono el modelo corsa classic");
+//break
+//case "2:Fiat Cronos" :
+//alert("Selecciono Fiat Cronos")
+//break;
+//case "3:Fiat Siena":
+//alert("Selecciono el modelo Fiat Siena");
+//break
+//case "4: Gol Power":
+//alert("Selecciono el modelo Gol Power");
+//  break
+//    default:return "Opcion no valida"
+//if (nombre != "" && vehiculo1) {
+//  console.log("Bienvenido" + " " + nombre + " " + "a viajero Motor");
+//} else {
+//  alert("Error, debes cargar todos los datos.")
+//}
+
+
